8dc52c2804c98f9b9d8697835e4ee80f53b72fd4
Shanmuganand <albumupdated@gmail.com>
2018-01-31 15:07:22 +0400

    Shan
    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'to Node' });
+  res.render('index', { title: 'Node Express' });
 });
 
 module.exports = router;
